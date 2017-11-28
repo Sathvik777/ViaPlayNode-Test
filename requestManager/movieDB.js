f684d46d84e5ffd5866795c2be154a77ed60150d
LAPTOP-2QHCPVP2\sathvik <sathvik.katam@mobileinteraction.se>
2017-11-28 17:46:59 +0100

    removed debug logs

--- requestManager/movieDB.js
+++ requestManager/movieDB.js
@@ -1,25 +1,25 @@
 
 var Q = require('q');
-var client = require('request-json').createClient('https://api.themoviedb.org/3/');
+let config = require('../config');
+const themoviedbBaseUrl =  process.env.MOVIEDB_BASE_URL || config.themoviedbBaseUrl;
+var client = require('request-json').createClient(themoviedbBaseUrl);
 var youtubeBaseUrl = "https://www.youtube.com/watch?v=";
 var fallbackYouTubeId = "";
-var apiKey = process.env.MOVIEDB_KEY;
+var apiKey = process.env.MOVIEDB_KEY || config.movieDBapiKey;
 
 module.exports = {
   /**
-  Function takes IMDB movie ID 
+  Function takes IMDB movie ID
   returns a Promise which makes sure to stay in sync and provide the movie db ID
   */
   getMovieDBID: function(movieImdbId) {
-    console.log(apiKey);
     return Q.Promise(function(resolve, reject) {
       client.get('find/'+movieImdbId+'?api_key='+apiKey+'&external_source=imdb_id', function (err, res, body) {
         if (err || res.statusCode > 300) {
-          
+
           console.log(body);
           reject(new Error());
         } else {
-          console.log(body["movie_results"][0].id);
           let movieBDId = body["movie_results"][0].id;
           resolve(movieBDId);
         }
@@ -30,22 +30,19 @@ module.exports = {
   Function takes the movie db ID
   returns a Promise which makes sure to stay in sync and provide Yoube Trailer ID
   */
-  getMovieDBTrailer: function(movieDBId) {
-    console.log(apiKey);
+  getMovieDBTrailerID: function(movieDBId) {
     return Q.Promise(function(resolve, reject) {
       client.get('movie/'+movieDBId+'?api_key='+apiKey+'&append_to_response=trailers', function (err, res, body) {
         if (err || res.statusCode > 300) {
-          // TODO
-          
           console.log(body);
           reject(new Error());
         } else {
-          console.log(body.trailers.youtube[0].source);
-          const youtubeVideoId = body.trailers.youtube[0].source;
-          resolve(`https://www.youtube.com/watch?v=${youtubeVideoId}`);
+          let youtubeVideoId = body.trailers.youtube[0].source;
+          if(youtubeVideoId ==  undefined) reject(new Error());
+          resolve(youtubeVideoId);
         }
       });
     });
   }
-  
+
 }
