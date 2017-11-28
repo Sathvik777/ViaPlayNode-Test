45a07527c86753acbb803a3e92cedabe9f5df0c4
LAPTOP-2QHCPVP2\sathvik <sathvik.katam@mobileinteraction.se>
2017-11-28 18:21:34 +0100

    removed text search

--- requestManager/viaPlay.js
+++ requestManager/viaPlay.js
@@ -5,25 +5,6 @@ var viaPlayBaseUrl = process.env.VIAPLAY_BASE_URL || config.viaPlayBaseUrl;
 var client = require('request-json').createClient(viaPlayBaseUrl);
 var movieDB = require('./movieDB');
 module.exports = {
-  /**
-  Get search text and makes resquest to content.viaplay.se endpoint
-  Takes Movie name provides IMDB ID
-  */
-  getMovieIMDBFromText: function(movie) {
-    console.log(movie);
-    return Q.Promise(function(resolve, reject) {
-      client.get(viaPlayBaseUrl+'pc-se/film/'+movie, function (err, res, body) {
-        if (err || res.statusCode > 300) {
-
-          reject(new Error());
-        } else if(body.hasOwnProperty('_embedded')) {
-          resolve(body._embedded["viaplay:blocks"][0]._embedded["viaplay:product"].content.imdb.id);
-        } else {
-          reject(new Error());
-        }
-      });
-    });
-  },
    /**
    Gets endpoints url and makes request to that endpoint
   Takes ViaPlayUrl provides IMDB ID
--- server.js
+++ server.js
@@ -19,39 +19,6 @@ app.get("/", function (request, response) {
 
 
 /**
-  GET endpoint takes Query Param `search` as movie name to search Viaplay endpoint and find Youtube Trailer Link
-*/
-
-app.get("/viaplay-trailer-gen/text", function (request, response, next) {
-  /**
-    Using Promises to avoid callback-hell
-  */
-  return  viaplay.getMovieIMDBFromText(request.query.search)
-      .then( function(movieImdbId){
-        return movieDB.getMovieDBID(movieImdbId);
-    }).then( function(movieDBId){
-        return movieDB.getMovieDBTrailerID(movieDBId);
-    }).then( function(youtubeVideoId) {
-      var trailerUrl ;
-        if(request.query.embed){
-          trailerUrl = youtubeEmbedUrl +''+ youtubeVideoId;
-      } else{
-          trailerUrl = youtubeWatchUrl +''+ youtubeVideoId;
-      }
-      response.statusCode = 200;
-      response.json({ youtubeTrailerLink: trailerUrl });
-      return;
-    }).catch(function(err) {
-      response.statusCode = 400;
-      response.json({ error: "Movie Not Found" });
-      return;
-    }).done(function() {
-    response.end();
-    });
-    next();
-});
-
-/**
   POST endpoint takes Json Request body find Youtube Trailer Link
 */
 app.post("/viaplay-trailer-gen/url", function (request, response, next) {
