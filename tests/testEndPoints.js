e4e05c1a1453f7a1777fd7012e9db5c5266be5bf
LAPTOP-2QHCPVP2\sathvik <sathvik.katam@mobileinteraction.se>
2017-11-28 18:45:15 +0100

    new test to verify the value

--- tests/testEndPoints.js
+++ tests/testEndPoints.js
@@ -42,3 +42,22 @@ describe('/POST url Negative', () => {
           });
     });
 });
+
+describe('/POST url To test if response value is correct', () => {
+    it('should return Arrival Trailer link', (done) => {
+        let requestBody = {
+            "url" : "https://content.viaplay.se/pc-se/film/arrival-2016"
+        }
+        let expectedvalue = "https://www.youtube.com/watch?v=gwqSi_ToNPs";
+      chai.request(server)
+          .post('/viaplay-trailer-gen/url')
+          .send(requestBody)
+          .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('youtubeTrailerLink');
+              res.body.youtubeTrailerLink.should.equal(expectedvalue)
+            done();
+          });
+    });
+});
